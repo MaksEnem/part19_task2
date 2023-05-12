3ffb8a7cdf14b75214be705cb00e933896aaf01b
MaksEnem <110655471+MaksEnem@users.noreply.github.com>
2023-05-12 20:19:26 +1100

    'finish'

--- task2.cpp
+++ task2.cpp
@@ -7,7 +7,7 @@ int main() {
 	std::ifstream fin_w;
 	char buffer[20];
 
-	fin_w.open("D:\\Skillbox\\C++\\working\\Practice 19 (reading from files)\\task2\\words.txt");
+	fin_w.open("D:\\Skillbox\\C++\\working\\Practice 19 (reading from files)\\task2\\words.txt", std::ios::binary);
 
 	if (fin_w.is_open()) {
 
@@ -22,33 +22,18 @@ int main() {
 		std::cout << "File is not open\n";
 
 	}
-
-	
-	do {
+		
+	while (!fin_w.eof()) {
 
 		fin_w.read(buffer, sizeof(buffer));
 
-		for (int i = 0; i < 20; ++i) {
+		for (int i = 0; i < fin_w.gcount(); ++i) {
 
 			std::cout << buffer[i];
 
 		}
-
-
-	} while (!fin_w.eof());
 		
-
-
+	}	
 	
-
-	
-
-	
-
-	
-
-
-
-
 	fin_w.close();
 }
